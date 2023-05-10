529baaf26e8312eb5622492b8e33f8ccf5778303
XiaooooooBawang <junzhizou19@163.com>
2023-05-11 00:13:55 +0800

    Update main.c

--- pwm-generator/SDK/main.c
+++ pwm-generator/SDK/main.c
@@ -1,32 +1,24 @@
 #include "xparameters.h"
 #include "PWM_Generator.h"
-#include "xil_printf.h"
-#include <stdio.h>
 
-/*pwm generator的AXI总线基地址*/
+/*pwm generator AXI base address*/
 #define XPAR_PWM_GENERATOR_0_S00_AXI_BASEADDR 0x43c00000
 
 int main(void)
 {
-	init_platform();
-
-	/* GPIO driver initialization */
-	int status;
-	status = XGpio_Initialize(&Gpio, GPIO_DEVICE_ID);
-	if (status != XST_SUCCESS) {
-		printf("Gpio Initialization Failed\r\n");
-		return XST_FAILURE;
-	}
-
-
-	// 20ms = 2000000 * 10ns
-	int period = 2000000;
+	/* 
+        The default clock period for zynq is 10ns.
+        In this example, my servo is controlled by pwm, 
+        and the period of this servo is 20ms, so I set prd as 2000000.
+        20ms = 2000000 * 10ns
+     */
+	int prd = 2000000;
 
 	PWM_GENERATOR_mDisableOutput(XPAR_PWM_GENERATOR_0_S00_AXI_BASEADDR);
-	PWM_GENERATOR_mSetLoad(XPAR_PWM_GENERATOR_0_S00_AXI_BASEADDR, period);
-    PWM_GENERATOR_mSetCompare(XPAR_PWM_GENERATOR_0_S_AXI_BASEADDR, 150000); 
-    PWM_GENERATOR_mEnableOutput(XPAR_PWM_GENERATOR_0_S_AXI_BASEADDR);
+	PWM_GENERATOR_mSetLoad(XPAR_PWM_GENERATOR_0_S00_AXI_BASEADDR, prd);
+    PWM_GENERATOR_mSetCompare(XPAR_PWM_GENERATOR_0_S00_AXI_BASEADDR, 150000); // set the duty cycle
+    PWM_GENERATOR_mEnableOutput(XPAR_PWM_GENERATOR_0_S00_AXI_BASEADDR);
 	while(1)
-	cleanup_platform();
+	
 	return 0;
 }
